5230529e4e7c7bd1250d60941cbc7ab572fe5c29
adam wong <adamwong246@gmail.com>
2021-01-27 09:23:01 -0800

    update

--- item.lua
+++ item.lua
@@ -1,49 +1,48 @@
 --item.lua
 
--- local fireArmor = table.deepcopy(data.raw["armor"]["heavy-armor"]) -- copy the table that defines the heavy armor item into the fireArmor variable
+local fireArmor = table.deepcopy(data.raw["armor"]["heavy-armor"]) -- copy the table that defines the heavy armor item into the fireArmor variable
 
--- fireArmor.name = "fire-armor"
--- fireArmor.icons = {
---   {
---     icon = fireArmor.icon,
---     tint = {r=1,g=0,b=0,a=0.3}
---   },
--- }
+fireArmor.name = "fire-armor"
+fireArmor.icons = {
+  {
+    icon = fireArmor.icon,
+    tint = {r=1,g=0,b=0,a=0.3}
+  },
+}
 
--- fireArmor.resistances = {
---   {
---     type = "physical",
---     decrease = 6,
---     percent = 10
---   },
---   {
---     type = "explosion",
---     decrease = 10,
---     percent = 30
---   },
---   {
---     type = "acid",
---     decrease = 5,
---     percent = 30
---   },
---   {
---     type = "fire",
---     decrease = 0,
---     percent = 100
---   }
--- }
+fireArmor.resistances = {
+  {
+    type = "physical",
+    decrease = 6,
+    percent = 10
+  },
+  {
+    type = "explosion",
+    decrease = 10,
+    percent = 30
+  },
+  {
+    type = "acid",
+    decrease = 5,
+    percent = 30
+  },
+  {
+    type = "fire",
+    decrease = 0,
+    percent = 100
+  }
+}
 
--- local recipe = table.deepcopy(data.raw["recipe"]["heavy-armor"])
--- recipe.enabled = true
--- recipe.name = "fire-armor"
--- recipe.ingredients = {{"copper-plate",200},{"steel-plate",50}}
--- recipe.result = "fire-armor"
+local fireArmorRecipe = table.deepcopy(data.raw["recipe"]["heavy-armor"])
+fireArmorRecipe.enabled = true
+fireArmorRecipe.name = "fire-armor"
+fireArmorRecipe.ingredients = {{"copper-plate",200},{"steel-plate",50}}
+fireArmorRecipe.result = "fire-armor"
 
--- data:extend{fireArmor,recipe}
 
 local footGenerator = table.deepcopy(data.raw["item"]["exoskeleton-equipment"])
-
 footGenerator.name = "foot-generator"
+footGenerator.description = "foot-generator"
 footGenerator.icons = {
   {
     icon = footGenerator.icon,
@@ -51,10 +50,14 @@ footGenerator.icons = {
   },
 }
 
-local recipe2 = table.deepcopy(data.raw["recipe"]["exoskeleton-equipment"])
-recipe2.enabled = true
-recipe2.name = "foot-generator"
-recipe2.ingredients = {{"copper-plate",200},{"steel-plate",50}}
-recipe2.result = "foot-generator"
+local footGeneratorRecipe = table.deepcopy(data.raw["recipe"]["exoskeleton-equipment"])
+footGeneratorRecipe.enabled = true
+footGeneratorRecipe.name = "foot-generator"
+footGeneratorRecipe.description = "foot-generator"
+footGeneratorRecipe.ingredients = {{"copper-plate",200},{"steel-plate",50}}
+footGeneratorRecipe.result = "foot-generator"
 
-data:extend{footGenerator, recipe2}
+data:extend{fireArmor,fireArmorRecipe, footGenerator, footGeneratorRecipe}
+
+log(serpent.block(data.raw["item"]["foot-generator"]))
+log(serpent.block(data.raw["recipe"]["foot-generator"]))
